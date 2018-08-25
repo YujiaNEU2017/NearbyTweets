d779b91302fddf69d19e5ae3125432d65021d1ea
YujiaNEU2017 <31643613+YujiaNEU2017@users.noreply.github.com>
2018-08-25 13:48:04 -0700

    Update twitterConfigTest.js

--- server/twitterConfigTest.js
+++ server/twitterConfigTest.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumerKey: 'XAeMVAF1HyHUp95nKKKPajBLu',
-  consumerSecret: 'KMkm1krGr32FweNp5f7hLz6RUNZQRsLzgFcmnZWV9tM3gZKEsq',
-  guestTokenKey: '978769349237338112-VLrHTav1BUvwJQDyzEGmBcIi4gSnJcH',
-  guestTokenSecret: 'wvTYVSaxTxFaSzVVP8f6flvIWgLxOlwOHsZQfsYXViTOi',
+  consumerKey: '',
+  consumerSecret: '',
+  guestTokenKey: '',
+  guestTokenSecret: '',
   callbackUrl: 'http%3A%2F%2Flocalhost%3A3000',
   params: {
     q: '#',
